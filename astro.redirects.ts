58f7b021fb5b83f9176cc91fed0d6428a1881911
ArtemDzhereleiko <adzhereleiko@thingsboard.io>
2026-02-20 09:59:16 +0200

    Add redirect for new structure of pe product docs

--- astro.redirects.ts
+++ astro.redirects.ts
@@ -4,4 +4,10 @@ export const redirects: AstroUserConfig['redirects'] = {
 	// Add redirects here. They work in both dev and production.
 	// Example: '/old-path/': '/new-path/',
 	// Example with params: '/docs/[...slug]': '/new-docs/[...slug]',
+	'/docs/pe/edge/': '/docs/edge/pe/',
+	'/docs/pe/edge/[...slug]': '/docs/edge/pe/[...slug]',
+	'/docs/pe/mobile/': '/docs/mobile/pe/',
+	'/docs/pe/mobile/[...slug]': '/docs/mobile/pe/[...slug]',
+	'/docs/pe/mqtt-broker/': '/docs/mqtt-broker/pe/',
+	'/docs/pe/mqtt-broker/[...slug]': '/docs/mqtt-broker/pe/[...slug]',
 };
